8927d5424f1ac0972c3508ebf8f3c3b1b1ce04e1
Naman Shankhydhar <naman.geek@gmail.com>
2020-10-11 14:31:57 +0530

    removed warnings

--- src/components/ExportAsImg.js
+++ src/components/ExportAsImg.js
@@ -3,7 +3,7 @@ import {
   exportComponentAsJPEG,
   exportComponentAsPNG,
 } from "react-component-export-image";
-import ReactDOM from "react-dom";
+
 export class ExportAsImg extends Component {
   constructor(props) {
     super(props);
